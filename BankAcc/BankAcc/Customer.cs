cf6b4edbf128d2213c58901e30578b51d092db8f
kiaitk2k <kiaito2k@gmail.com>
2018-11-15 09:57:38 -0500

    Update BankAcc/BankAcc/Customer.cs
    building customer business object

---
+++ BankAcc/BankAcc/Customer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/******
+ * Author: :Levi Llewellyn 
+ * Created: November 2018
+ * Banp Application 
+ * Customer Business Object
+ * ****/
+namespace BankApp
+{
+    class Customer
+    {
+        string CustID;
+        string CustPW;
+        string CustFN;
+        string CustLN;
+        string CustADD;
+        string CustEmail;
+
+        //Constructor set the property values
+        public Customer()
+        {
+            CustID = "";
+            CustPW = "";
+            CustFN = "";
+            CustLN = "";
+            CustADD = "";
+            CustEmail = "";
+        }
+        //All args constructor
+        public Customer(string id, string pw, string fn, string ln, string add, string em)
+        {
+            CustID = id;
+            CustPW = pw;
+            CustFN = fn;
+            CustLN = ln;
+            CustADD = add;
+            CustEmail = em;
+        }
+
+        //Getters and Setters
+        public void setcustid(string id)
+        {
+            CustID = id;
+        }
+        public string getcustid()
+        {
+            return CustID;
+        }
+        public void setcustpw(string pw)
+        {
+            CustPW = pw;
+        }
+        public string getcustpw()
+        {
+            return CustPW;
+        }
+        public void setcustfn(string fn)
+        {
+            CustFN = fn;
+        }
+        public string getcustfn()
+        {
+            return CustFN;
+        }
+        public void setcustln(string ln)
+        {
+            CustLN = ln;
+        }
+        public string getcustln()
+        {
+            return CustLN;
+        }
+        public void setcustadd(string add)
+        {
+            CustADD = add;
+        }
+        public string getcustadd()
+        {
+            return CustADD;
+        }
+        public void setcustem(string em)
+        {
+            CustEmail = em;
+        }
+        public string getcustem()
+        {
+            return CustEmail;
+        }
+    }
+}
+public void display()
+{
+
+}
